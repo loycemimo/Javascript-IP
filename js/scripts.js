a6e2fdc782888023932383060fb97ec6a1070d5f
loycemimo <loycenjoroge.ln@gmail.com>
2021-03-07 20:08:53 +0300

    get gender from form to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
   var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+  function selectGender() {
+    var gender = document.getElementById("gender").value;
+    if (gender == "1") {
+      alert("Gender must be selected");
+      return false;
+    }
