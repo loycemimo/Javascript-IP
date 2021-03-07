dd235f709272d16bb8273152e5fb2203be46e61a
loycemimo <loycenjoroge.ln@gmail.com>
2021-03-07 20:06:09 +0300

    add arrays for days male and female names

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+  var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
