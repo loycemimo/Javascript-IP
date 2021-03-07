641cd997eafc59aa0e0e6c727aa66d8441984053
loycemimo <loycenjoroge.ln@gmail.com>
2021-03-07 20:24:45 +0300

    add changes in js  arrangment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-  var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
   function selectGender() {
     var gender = document.getElementById("gender").value;
@@ -39,9 +36,12 @@ function giveDay() {
         }  
         
         
-var monthNumber = parseInt(document.getElementById("monthBorn").value);
-var dayNumber = parseInt(document.getElementById("dateBorn").value);
-var yearNumber = parseInt(document.getElementById("yearBorn").value);
-var yearNumber = parseInt(document.getElementById("centuryBorn").value);
-
+var cc = parseInt(document.getElementById("centuryBorn").value.slice(0, 2));
+var yy = parseInt(document.getElementById("yearBorn").value.slice(2, 4));
+var dd = parseInt(document.getElementById("dayBorn").value);
+var mm = parseInt(document.getElementById("monthBorn").value);
 var day = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
