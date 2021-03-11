d7c7a213eaae4d92d66e60706111eb55206a631d
loycemimo <loycenjoroge.ln@gmail.com>
2021-03-11 18:52:55 +0300

    add final changes

---
+++ css/README.md
@@ -0,0 +1,15 @@
+# {AKAN NAME CALCULATOR}
+#### {Its an app that calculates the day of th wee you were born in and generates your akan name.}, {04/03/2021}
+#### By **{LOYCEmimo NJOROGE}**
+## Description
+{The user can get thier akan name and see what thier personality would be and represent thier day.}
+## Known Bugs
+{the calculator doesnot manage to generate the users akan name.}
+## Technologies Used
+{html, css, bootstrat, javascript}
+## Support and contact details
+{email:loycenjoroge.ln@gmail.com}
+### License
+*{MIT}*
+Copyright (c) {2021} 
+  
--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,100 @@
 
-  function selectGender() {
-    var gender = document.getElementById("gender").value;
-    if (gender == "1") {
-      alert("Gender must be selected");
-      return false;
+function results() {
+  var gender = parseInt(document.getElementById("gender").value)
+  var cc = parseInt(document.getElementById("centuryBorn").value.slice(0, 2));
+  var yy = parseInt(document.getElementById("yearBorn").value.slice(2, 4));
+  var dd = parseInt(document.getElementById("dateBorn").value);
+  var mm = parseInt(document.getElementById("monthBorn").value);
+  var day = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var akanDay = days[day.toFixed()];
+
+  var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+  var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+
+  if (gender == 1) {
+    if (akanDay === 'Sunday') {
+      document.getElementById("output").innerHTML = maleNames[0];
+      return true;
     }
-
-  function giveCentury() {
-        var century = document.getElementById("centuryBorn").value;
-        if (century == "") {
-          alert("Century must be filled out");
-          return false;
-        } 
-
-function giveYear() {
-        var year = document.getElementById("yearBorn").value;
-         if (year == "") {
-           alert("Year must be filled out");
-            return false;
-        }   
-
-function giveMonth() {
-         var month = document.getElementById("monthBorn").value;
-            if (month == "0") {
-              alert("Month must be selected");
-              return false;  
-        
-        }
-
-function giveDay() {
-         var day = document.getElementById("dateBorn").value;
-            if (day == "") {
-             alert("Day must be filled out");
-             return false;  
-        }  
-        
-        
-var cc = parseInt(document.getElementById("centuryBorn").value.slice(0, 2));
-var yy = parseInt(document.getElementById("yearBorn").value.slice(2, 4));
-var dd = parseInt(document.getElementById("dayBorn").value);
-var mm = parseInt(document.getElementById("monthBorn").value);
-var day = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
-
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+  }
+  else if (gender == 1) {
+    if (akanDay === 'Monday') {
+      document.getElementById("output").innerHTML = maleNames[1];
+      return true;
+    }
+  }
+  else if (gender == 1) {
+    if (akanDay === 'Tuesday') {
+      document.getElementById("output").innerHTML = maleNames[2];
+      return true;
+    }
+  }
+  else if (gender == 1) {
+    if (akanDay === 'Wednesday') {
+      document.getElementById("output").innerHTML= maleNames[3];
+      return true;
+    }
+  }
+  else if (gender == 1) {
+    if (akanDay === 'Thursday') {
+      document.getElementById("output").innerHTML = maleNames[4];
+      return true;
+    }
+  }
+  else if (gender == 1) {
+    if (akanDay === 'Friday') {
+      document.getElementById("output").innerHTML = maleNames[5];
+      return true;
+    }
+  }
+  else if (gender == 1) {
+    if (akanDay === 'Saturday') {
+      document.getElementById("output").innerHTML = maleNames[6];
+      return true;
+    }
+  }
+  if (gender == 2) {
+    if (akanDay === 'Sunday') {
+      document.getElementById("output").write= femaleNames[0];
+      return true;
+    }
+  }
+  else if (gender == 2) {
+    if (akanDay === 'Monday') {
+      document.getElementById("output").write= femaleNames[1];
+      return true;
+    }
+  }
+  else if (gender == 2) {
+    if (akanDay === 'Tuesday') {
+      document.getElementById("output").write= femaleNames[2];
+      return true;
+    }
+  }
+  else if (gender == 2) {
+    if (akanDay === 'Wednesday') {
+      document.getElementById("output").write= femaleNames[3];
+      return true;
+    }
+  }
+  else if (gender == 2) {
+    if (akanDay === 'Thursday') {
+      document.getElementById("output").write= femaleNames[4];
+      return true;
+    }
+  }
+  else if (gender == 2) {
+    if (akanDay === 'Friday') {
+      document.getElementById("output").write= femaleNames[5];
+      return true;
+    }
+  }
+  else if (gender == 2) {
+    if (akanDay === 'Saturday') {
+      document.getElementById("output").write= femaleNames[6];
+      return true;
+    }
+  }
+}
